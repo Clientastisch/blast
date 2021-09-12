3bf0aa0faa301081ebee0faf562609ae3c882e4d
Clientastisch <51415855+Clientastisch@users.noreply.github.com>
2021-09-12 18:17:54 +0200

    fix: update smtp mailer

--- src/main/java/me/micartey/blast/controller/EmailController.java
+++ src/main/java/me/micartey/blast/controller/EmailController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.micartey.blast.bodies.MailBody;
 import me.micartey.blast.mail.SmtpMailer;
-import me.micartey.blast.mail.SmtpMessage;
 import me.micartey.blast.responses.ErrorResponse;
 import me.micartey.blast.responses.Response;
 import org.springframework.http.ResponseEntity;
@@ -24,20 +23,22 @@ public class EmailController {
         log.info("preparing...");
 
         try {
-            new SmtpMessage(
-                    new SmtpMailer(
-                            mailBody.getHost(),
-                            mailBody.getPort(),
-                            mailBody.isStarttls()
-                    ).createSession(
-                            mailBody.getAuth().getMail(),
-                            mailBody.getAuth().getPassword()
-                    ),
+            SmtpMailer mailer = new SmtpMailer(
+                    mailBody.getHost(),
+                    mailBody.getPort(),
+                    mailBody.isStarttls()
+            );
+            
+            mailer.createSession(
                     mailBody.getAuth().getMail(),
+                    mailBody.getAuth().getPassword()
+            );
+
+            mailer.sendMessage(
                     mailBody.getMessage().getRecipient(),
                     mailBody.getMessage().getSubject(),
                     mailBody.getMessage().getText()
-            ).send();
+            );
         } catch(MessagingException exception) {
             log.error("Something went wrong: " + exception);
             return ResponseEntity.internalServerError().body(new ErrorResponse(exception.getMessage()));
--- src/main/java/me/micartey/blast/mail/SmtpMailer.java
+++ src/main/java/me/micartey/blast/mail/SmtpMailer.java
@@ -1,13 +1,18 @@
 package me.micartey.blast.mail;
 
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 public class SmtpMailer {
 
     private final Properties properties;
+
+    private PasswordAuthentication authentication;
+    private Session                session;
 
     public SmtpMailer(String host, int port, boolean starttls) {
         this.properties = new Properties();
@@ -17,15 +22,36 @@ public class SmtpMailer {
         this.properties.put("mail.smtp.port", String.valueOf(port));
     }
 
-    public Session createSession(String mail, String password) {
-        return Session.getInstance(this.properties, new Authenticator() {
+    public void createSession(String mail, String password) {
+        this.authentication = new PasswordAuthentication(
+                mail,
+                password
+        );
+
+        this.session = Session.getInstance(this.properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(
-                        mail,
-                        password
-                );
+                return authentication;
             }
         });
+    }
+
+    public void sendMessage(String recipient, String subject, String text) throws MessagingException {
+        Message message = new MimeMessage(this.session);
+        message.setFrom(new InternetAddress(this.authentication.getUserName()));
+        message.setRecipients(
+                Message.RecipientType.TO,
+                InternetAddress.parse(recipient)
+        );
+        message.setSubject(subject);
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(text, "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+        Transport.send(message);
     }
 }
